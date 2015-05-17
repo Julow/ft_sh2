dd0c8b287b5549ef2ca22ef835a4cc210b852063
juloo <juloo.dsi@gmail.com>
2015-05-17 23:47:22 +0200

    Implement builtin eval

--- srcs/builtins/builtin_eval.c
+++ srcs/builtins/builtin_eval.c
@@ -6,24 +6,30 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/15 23:06:49 by juloo             #+#    #+#             */
-/*   Updated: 2015/05/17 23:42:52 by juloo            ###   ########.fr       */
+/*   Updated: 2015/05/17 23:46:01 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "exec.h"
 #include "builtin.h"
+#include <stdlib.h>
 
 int				builtin_eval(t_msh *sh, int argc, char **argv)
 {
+	t_string		eval;
+	t_sub			tmp;
 	int				i;
 
-	PS("eval");
+	ft_stringini(&eval);
 	i = 0;
 	while (++i < argc)
 	{
-		PC(' ');
-		PS(argv[i]);
+		if (i > 1)
+			ft_stringaddc(&eval, ' ');
+		ft_stringadd(&eval, argv[i]);
 	}
-	NL;
+	tmp = SUB(eval.content, eval.length);
+	exec_line(sh, &tmp);
+	free(eval.content);
 	return (0);
-	(void)sh;
 }
